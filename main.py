67ad51aad49dd9a37b3387a0ff2e297e50866fbc
edublanco <edublanco2011@hotmail.com>
2024-09-03 18:20:17 -0600

    first commit

---
+++ main.py
@@ -0,0 +1,9 @@
+# Tema: Two Pointers
+# Comentarios: 
+# Fecha: 2020/10/20
+# Autor: Max
+# liga: https://www.interviewbit.com/problems/remove-duplicates-from-sorted-array/
+
+# Problema: Remove Duplicates from Sorted Array
+
+# Solution:
